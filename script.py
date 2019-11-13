b2972dedd42be132c635018ccf77ededeee5c991
paulinusia <padamski@luc.edu>
2019-11-13 13:28:56 -0600

    changes to script.py

--- script.py
+++ script.py
@@ -11,38 +11,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def preprocess():
-    #load data
-    breast_data = pd.read_csv('./data/data.csv')
-    
 
 
-
-    
-    #drop diagnosis
-    y = breast_data['diagnosis']
-    x = breast_data.drop('diagnosis', axis=1)
-    
-    #replace M and B with 1s and 0s
-    y = y.replace(['M', 'B'], [1, 0])
-
-    columns = x.columns
-
-    #standardize the dataset to have a mean of 0, allows us to compare different scales
-    scaler = StandardScaler()
-
-    standardized_data = x.copy()
-    standardized_data[columns] = pd.DataFrame(scaler.fit_transform(standardized_data[columns]))
-
-
-    for col in x.columns:
-        #replace missing values with nAn
-        x[col].replace(0, np.NaN)
-        #print(x[col])
-        #replace nAn with mean of each column
-        x[col].fillna(x[col].mean(), inplace=True)
-        print(x[col])
-
+def SVM():
+    '''
+    implement here- or in different file
+    f1 regularization since data is imbalanced
+    '''
    
 
 
@@ -50,8 +25,43 @@ def preprocess():
 
 
 if __name__ == '__main__':
-    preprocess()
+    
+    '''
+    Pre-processing
 
+    data loaded, diagnosis dropped and converted to numerical data
+    All 0 data points are replaced with the mean for that column
+    Data is standardized
+
+    Dataset is imbalanced.
+        1. implement confusion matrix to show how well the model is doing
+        2. undersampling (majority) vs oversampling (minority) visualizations
+        3. kLearn's resample
+        4. SMOTE method 
+    
+    '''
+    
+    breast_data = pd.read_csv('./data/data.csv')
+
+    #drop diagnosis
+    y = breast_data['diagnosis']
+    x = breast_data.drop('diagnosis', axis=1)
+
+    #replace M and B with 1s and 0s
+    y = y.replace(['M', 'B'], [1, 0])
+    columns = x.columns
+
+    #replace missing values with mean
+    for col in x.columns:
+        x[col].replace(0, np.NaN)
+        x[col].fillna(x[col].mean(), inplace=True)
+        #print(x[col])
+
+    #standardize the dataset to have a mean of 0, allows us to compare different scales
+    scaler = StandardScaler()
+    standardized_data = x.copy()
+
+    standardized_data[columns] = pd.DataFrame(scaler.fit_transform(standardized_data[columns]))
 
 
 
